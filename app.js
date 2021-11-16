48725d3c496daa395812716e297d0218dffc5169
wamola-888 <com.com.development@gmail.com>
2021-11-16 13:56:43 +0000

    階乗を実装

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-'use strict';
+"use strict";
 /**
-* 与えられた自然数の階乗を返す
-* 階乗とは、1からその与えられた自然数までの数をすべてかけたものです
-* @param {Number} n
-* @returns {Number}
-*/
+ * 与えられた自然数の階乗を返す
+ * 階乗とは、1からその与えられた自然数までの数をすべてかけたものです
+ * @param {Number} n
+ * @returns {Number}
+ */
 function factorial(n) {
-    let result = 1;
-    // TODO このコメントを消して正しく実装してください。
-    return result;
+  let result = 1;
+  // TODO このコメントを消して正しく実装してください。
+  let tmp = 1;
+  while (tmp < n) {
+    tmp += 1;
+    result *= tmp;
+  }
+  return result;
 }
-const assert = require('assert');
-assert.equal(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1) }でした`);
-assert.equal(factorial(2), 2, `2の階乗は2ですが、実際は${factorial(2) }でした`);
-assert.equal(factorial(3), 6, `3の階乗は6ですが、実際は${factorial(3) }でした`);
-assert.equal(factorial(10), 3628800, `10の階乗は3628800ですが、実際は${factorial(10) }でした`);
-console.log('すべてのテストを通過しました');
+const assert = require("assert");
+assert.equal(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1)}でした`);
+assert.equal(factorial(2), 2, `2の階乗は2ですが、実際は${factorial(2)}でした`);
+assert.equal(factorial(3), 6, `3の階乗は6ですが、実際は${factorial(3)}でした`);
+assert.equal(
+  factorial(10),
+  3628800,
+  `10の階乗は3628800ですが、実際は${factorial(10)}でした`
+);
+console.log("すべてのテストを通過しました");
